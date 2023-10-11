64419747892cc468f85b1344560725919c8d50ba
Rahul Kumar <rahulkumarkvbr@gmail.com>
2023-10-11 11:54:09 +0530

    Added all operations possible in a linked list

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -14,6 +14,7 @@ void count_of_nodes(Node* node) {
         std::cout << temp->data << " ";
         temp = temp->next;
     }
+    std::cout << std:: endl;
 }
 
 Node* add_at_end(Node* tail, int num) {
@@ -24,12 +25,58 @@ Node* add_at_end(Node* tail, int num) {
     return new_node;
 }
 
-Node* add_at_beg(Node* head, int num) {
+void add_at_beg(Node** head, int num) {
     Node* new_node = new Node();
     new_node->data = num;
-    new_node->next = head;
-    head = new_node;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+void add_at_pos(Node* head, int position, int data) {
+    Node* ptr = head;
+    Node* new_node = new Node();
+    new_node->data = data;
+    new_node->next = nullptr;
+    position--;
+    while(position!=1) {
+        ptr = ptr->next;
+        position--;
+    }
+    new_node->next = ptr->next;
+    ptr->next = new_node;
+}
+
+Node* delete_at_beg(Node *head) {
+    Node* temp = head;
+    head = head->next;
+    free(temp);
     return head;
+}
+
+Node* delete_at_end(Node* head) {
+    Node* temp2 = head;
+    Node* temp = head;
+    while(temp->next!=nullptr) {
+        temp2 = temp;
+        temp = temp->next;
+    }
+    temp2->next = nullptr;
+    free(temp);
+    temp = nullptr;
+    return head;
+}
+
+void del_at_pos(Node* head, int position) {
+    // Node* temp2 = head;
+    Node *temp = head;
+    position-=2;
+    while(position != 0) {
+        temp = temp->next;
+        position--;
+    }
+    Node *temp2 = temp->next;
+    temp->next = temp->next->next;
+    free(temp2);
 }
 
 int main()
@@ -40,19 +87,13 @@ int main()
 
     Node *tail = add_at_end(head,98);
     tail = add_at_end(tail,3);
-    head = add_at_beg(head, 4);
+    add_at_beg(&head, 4);
+    add_at_pos(head, 2, 23);
+    auto* new_head = delete_at_beg(head);
+    new_head = delete_at_end(new_head);
+    del_at_pos(new_head,2);
+    Node* ptr = new_head;
 
-    count_of_nodes(head);
+    count_of_nodes(ptr);
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
